8f24113cb2146a2e36d9947c2f0c9d82a98d9833
Asva <ontrew@gmail.com>
2018-04-14 00:26:03 +0300

    Minor

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import BlockPanel from '../demo/components/Visual/BlockPanel/BlockPanel.vue'
+import BlockPanel from './demo/components/Visual/BlockPanel/BlockPanel.vue'
 import DoublePanel from './DoublePanel/DoublePanel.vue'
 import panelMixin from './DoublePanel/emitter/panelMixin'
 import DoublePanelItems from './DoublePanel/classes/DoublePanelItems'
